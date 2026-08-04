df23d58ecb03e82f648b92e90333e8a81fdb69db
Vitor Pereira <vitor.soares@idtech.org.br>
2026-08-04 16:55:03 -0300

    feat: Update Dockerfile to use base-laravel:8.4 image

--- config/jwt.php
+++ config/jwt.php
@@ -2,7 +2,7 @@
 
 return [
     'secret' => env('JWT_SECRET', ''),
-    'issuer' => env('JWT_ISSUER', 'http://auth-service:8003'),
+    'issuer' => env('JWT_ISSUER', 'http://tcc-auth-service:8003'),
     'audience' => env('JWT_AUDIENCE', 'tcc-api-gateway'),
     'ttl_minutes' => env('JWT_TTL_MINUTES', 15),
 ];
